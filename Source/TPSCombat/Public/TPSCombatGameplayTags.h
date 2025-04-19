de2c04b1b1793b1078e27b37edf5ebdedebc1502
Emirhan FEYZULLAHOĞLU <emirhan.feyzullahoglu@gmail.com>
2025-04-20 02:47:13 +0900

    tags

---
+++ Source/TPSCombat/Public/TPSCombatGameplayTags.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace TPSCombatGameplayTags
+{
+	TPSCOMBAT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	TPSCOMBAT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
