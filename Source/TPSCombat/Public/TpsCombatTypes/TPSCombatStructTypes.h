456896a93088e0e7c722b296b60255e63c588cf6
Emirhan FEYZULLAHOĞLU <emirhan.feyzullahoglu@gmail.com>
2025-04-29 15:38:51 +0900

    feat: basic ui setup complete

--- Source/TPSCombat/Public/TpsCombatTypes/TPSCombatStructTypes.h
+++ Source/TPSCombat/Public/TpsCombatTypes/TPSCombatStructTypes.h
@@ -39,4 +39,7 @@ struct FTPSCombatHeroWeaponData
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	FScalableFloat WeaponBaseDamage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSoftObjectPtr<UTexture2D> SoftWeaponIconTexture;
 };
